364c161a106a76947acea6d600b39f3915717ec4
kousha <kousha.ghodsizad@gmail.com>
2020-05-27 02:07:43 +0300

    git push

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
+	kernel "github.com/I-Reven/Hexagonal/src/applications/core"
 	"github.com/I-Reven/Hexagonal/src/applications/core/console"
+	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-colorable"
+	"os"
 )
+
+func init() {
+	setEnv()
+	setOsArg()
+}
 
 func main() {
 	boot()
 	console.Cli()
+}
+
+func boot() {
+	kernel.Boot()
+}
+
+func setOsArg() {
+	if os.Getenv("AUTO_SERVE") == "true" && len(os.Args) == 1 {
+		os.Args = []string{os.Args[0], "serve"}
+	}
+}
+
+func setEnv() {
+	var err error
+	env := os.Getenv("APP_ENV")
+
+	switch env {
+	case "production":
+		err = godotenv.Load(".live.env")
+	case "testing":
+		err = godotenv.Load(".test.env")
+	default:
+		err = godotenv.Load(".local.env")
+	}
+
+	if err != nil {
+		panic(err)
+	}
 }
