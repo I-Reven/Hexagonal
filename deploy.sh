380782ad777f027e7e30e6fde569299cc57e0a32
kousha ghodsizad <kousha.ghodsizad@gmail.com>
2020-05-29 23:29:27 +0300

    fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 
-echo Please select your deployment state? local, test, live, down
-
+echo Please select your deployment state? local, test, live
 read state
 
 if [ $state == "live" ]; then
-docker-compose config > docker-compose-deploy.yaml && kompose convert -f docker-compose-deploy.yaml --out ./k8s
-kubectl create -f ./k8s --save-config
-#kubectl apply -f ./k8s
+  echo Please select your deployment state? up, down, build
+  read state
+
+  if [ $state == "build" ]; then
+    docker-compose config > docker-compose-deploy.yaml && kompose convert -f docker-compose-deploy.yaml --out ./k8s
+    rm -rvf docker-compose-deploy.yaml
+  fi
+
+  if [ $state == "up" ]; then
+    kubectl create -f ./k8s --save-config
+  fi
+
+  if [ $state == "down" ]; then
+    kubectl delete -f ./k8s
+  fi
 fi
 
 if [ $state == "test" ]; then
-docker-compose up -D mongodb elassandra redis rabbitmq
+  echo Please select your deployment state? up, down
+  read state
+
+  if [ $state == "up" ]; then
+    docker-compose up -D mongodb elassandra redis rabbitmq
+  fi
+
+  if [ $state == "down" ]; then
+    docker-compose down
+  fi
 fi
 
 if [ $state == "local" ]; then
-docker-compose up -build
-fi
-
-if [ $state == "down" ]; then
-kubectl delete -f ./k8s
-docker-compose down
+  docker-compose up --build
 fi
 
 
